192bab4a092fe4fb42237b4eafd628bd33bc2ef7
lohithadevi <lohithadevi1835@gmail.com>
2026-02-03 23:43:18 +0530

    Added leetcode problem - FEB 03

---
+++ Leetcode/3637.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isTrionic(int[] nums) {
+      
+        int n = nums.length;
+        if (n < 4) return false;
+
+        int i = 0;
+
+        while (i + 1 < n && nums[i] < nums[i + 1]) i++;
+        if (i == 0) return false;
+
+        int inc = i;
+
+        while (i + 1 < n && nums[i] > nums[i + 1]) i++;
+        if (i == inc) return false;
+
+        int dec = i;
+
+        while (i + 1 < n && nums[i] < nums[i + 1]) i++;
+
+        return dec < n - 1 && i == n - 1;
+ 
+
+    }
+}
