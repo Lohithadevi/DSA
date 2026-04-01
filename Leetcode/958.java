146ab0428fa60141562dc04d6d56265dcc677a7d
lohithadevi <lohithadevi1835@gmail.com>
2026-04-02 00:50:52 +0530

    APR 01

---
+++ Leetcode/958.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isCompleteTree(TreeNode root) {
+        Queue<TreeNode> q=new LinkedList<>();
+        boolean check=false;
+        q.offer(root);
+        while(!q.isEmpty())
+        {
+            TreeNode curr=q.poll();
+            if(curr==null)
+            {
+                check=true;
+            }
+            else
+            {
+                if(check) return false;
+                q.add(curr.left);
+                q.add(curr.right);
+            }
+        } 
+        return true;
+    }
+}
