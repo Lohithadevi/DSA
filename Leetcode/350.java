6cd9f1f17c357309a6ff07e7ef335b1a19ade9cc
lohithadevi <lohithadevi1835@gmail.com>
2026-04-12 22:33:06 +0530

    APR 12

---
+++ Leetcode/350.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        ArrayList<Integer> list=new ArrayList<>();
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int p1=0;
+        int p2=0;
+        while(p1 < nums1.length && p2 < nums2.length)
+        {
+            if(nums1[p1]==nums2[p2])
+            {
+                list.add(nums1[p1]);
+                p1++;
+                p2++;
+                continue;
+            }
+            if(nums1[p1]<nums2[p2])
+            {
+                p1++;
+            }
+            else
+            {
+                p2++;
+            }
+        }
+        int res[]=new int[list.size()];
+        for(int i=0;i<list.size();i++)
+        {
+            res[i]=list.get(i);
+        }
+        return res;
+    }
+}
