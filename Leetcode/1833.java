a3b5c0b7bb36dd236f3e61d795e671b9461701a2
lohithadevi <lohithadevi1835@gmail.com>
2026-06-21 22:50:37 +0530

    JUN 21

---
+++ Leetcode/1833.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        int res=0;
+        Arrays.sort(costs);
+        for(int i=0;i<costs.length;i++)
+        {
+            if(costs[i]<=coins)
+            {
+                res++;
+            }
+            else break;
+            coins-=costs[i];
+        }
+        return res;
+    }
+}
