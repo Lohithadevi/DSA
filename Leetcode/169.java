1f1b33156b10143df4c7159113dc9600003ef7a5
lohithadevi <lohithadevi1835@gmail.com>
2026-07-03 23:11:11 +0530

    JULY 03

--- Leetcode/169.java
+++ Leetcode/169.java
@@ -1,24 +1,30 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int stop=nums.length/2;
-        Arrays.sort(nums);
-        if(nums.length==1 || nums.length==2) return nums[0];
-        int i=0;
-        int j=1;
-        while(j<nums.length)
+        int count=0;
+        int curr=0;
+        for(int n : nums)
         {
-            int c=1;
-            while(j<nums.length && nums[i]==nums[j])
-            {
-                j++;
-                c++;
+            if(count==0){
+                curr=n;
             }
-         
-            if(c>stop) return nums[i];
-            i=j;
-            j++;
-       
+            if(curr==n)
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+            }
         }
-        return nums[0];
+        return curr;
     }
 }
+
+
+
+
+
+
+
+
+
